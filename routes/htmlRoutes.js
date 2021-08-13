6c778af71510d102cde2946e41d738edae253077
natnaelh14 <natnaelh14@github.com>
2021-08-12 23:59:36 -0400

    created html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+const Router = require("express").Router;
+const path = require("path");
+
+const htmlRoutes = new Router();
+
+   htmlRoutes.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    htmlRoutes.get("/exercise", function(req,res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    htmlRoutes.get("/stats", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    })
+
+module.exports = htmlRoutes;
